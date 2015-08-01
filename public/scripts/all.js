df00eb71aba5a3e99f48b0ecde7db3a26aecf037
Moisés Pio <live.moises@gmail.com>
2015-08-01 17:30:09 -0300

    Works carousel

--- public/scripts/all.js
+++ public/scripts/all.js
@@ -1,7 +1,15 @@
 var app = angular.module('app', []);
-app.controller('mainController', function($scope) {
+app.controller('mainController', function($scope, $timeout) {
 	$('#presentation h1').typed({
 		strings: ['I design stuff.'],
 		typeSpeed: 100
+	});
+
+	$timeout(function () {
+		$('.carousel').owlCarousel({
+			items: 1,
+			nav: true,
+			navSpeed: 500
+		});
 	});
 });
--- sources/scripts/controllers/mainController.js
+++ sources/scripts/controllers/mainController.js
@@ -1,6 +1,14 @@
-app.controller('mainController', function($scope) {
+app.controller('mainController', function($scope, $timeout) {
 	$('#presentation h1').typed({
 		strings: ['I design stuff.'],
 		typeSpeed: 100
+	});
+
+	$timeout(function () {
+		$('.carousel').owlCarousel({
+			items: 1,
+			nav: true,
+			navSpeed: 500
+		});
 	});
 });
