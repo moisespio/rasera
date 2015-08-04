74699c059197e9148de8522f1585693322c82ede
Rasera <Rasera@Raseras-iMac.local>
2015-08-04 02:11:55 -0300

    Skrollr script added

--- public/scripts/all.js
+++ public/scripts/all.js
@@ -1,19 +1,16 @@
-var app = angular.module('app', []);
-app.controller('mainController', function ($scope, $timeout) {
-    $('#presentation h1').typed({
-        strings: ['I design stuff.'],
-        typeSpeed: 110
-    });
-
-    $timeout(function () {
-        $('.carousel').owlCarousel({
-            items: 1,
-            nav: true,
-            navSpeed: 1200,
-            center: true,
-            URLhashListener: true,
-            startPosition: 'URLHash',
-            dots: false,
-        });
-    });
+$('#presentation h1').typed({
+    strings: ['I design stuff.'],
+    typeSpeed: 110
 });
+
+$('.carousel').owlCarousel({
+    items: 1,
+    nav: true,
+    navSpeed: 1200,
+    center: true,
+    URLhashListener: true,
+    startPosition: 'URLHash',
+    dots: false,
+});
+
+var s = skrollr.init();
--- sources/scripts/app.js
+++ sources/scripts/app.js
@@ -1,1 +1,16 @@
-var app = angular.module('app', []);
+$('#presentation h1').typed({
+    strings: ['I design stuff.'],
+    typeSpeed: 110
+});
+
+$('.carousel').owlCarousel({
+    items: 1,
+    nav: true,
+    navSpeed: 1200,
+    center: true,
+    URLhashListener: true,
+    startPosition: 'URLHash',
+    dots: false,
+});
+
+var s = skrollr.init();
