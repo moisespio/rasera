f6da87ab0987251069c155bdf5190e472d07f33f
Rasera <Rasera@Raseras-iMac.local>
2015-08-05 17:41:22 -0300

    Mobile version updates

--- sources/scripts/app.js
+++ sources/scripts/app.js
@@ -1,19 +1,19 @@
 $('#presentation h1').typed({
     strings: ['I design stuff.'],
-    typeSpeed: 110
+    typeSpeed: 120
 });
 
 $('.carousel').owlCarousel({
     items: 1,
     nav: true,
-    navSpeed: 1200,
+    navSpeed: 800,
     center: true,
     URLhashListener: true,
     startPosition: 'URLHash',
     dots: false,
 });
-/*
+
 var istouchdevice = 'ontouchstart' in document.documentElement;
 if (!istouchdevice) {
     var s = skrollr.init();
-};*/
+};
